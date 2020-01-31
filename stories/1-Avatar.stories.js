62e34a3e469033aa080e64354efc2f5bc3aec487
Sean Groff <sean.groff@selectquote.com>
2020-01-31 14:20:44 -0600

    replace rollup with parcel

--- stories/1-Avatar.stories.js
+++ stories/1-Avatar.stories.js
@@ -4,7 +4,7 @@ import {withKnobs, boolean} from '@storybook/addon-knobs';
 import {withInfo} from '@storybook/addon-info';
 import markdown from '../notes/1-Avatar.md';
 
-import Avatar from '../src/Avatar';
+import Avatar from '../src/components/Avatar';
 
 export default {
   title: 'Avatar',
