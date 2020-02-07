45be6a2562def60ce29ae3963fcec48bdcb21207
Chris Boyle <chris.boyle@selectquote.com>
2020-02-07 10:19:13 -0600

    changed the class component to functional

--- src/components/TabsWithOverflow/OverflowPopover.js
+++ src/components/TabsWithOverflow/OverflowPopover.js
@@ -1,63 +1,58 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Popover from 'material-ui/Popover/Popover';
 import classnames from 'classnames';
 import {Menu, MenuItem} from 'material-ui/Menu';
 import './OverflowPopover.css';
 
-class OverflowPopover extends PureComponent {
-  selectItem(value) {
-    const {selectOption, handleClose} = this.props;
-
+function OverflowPopover({
+  selectOption,
+  handleClose,
+  isOpen,
+  anchorEl,
+  options,
+  selectedOption,
+  menuStyle,
+}) {
+  const selectItem = value => {
     selectOption(value);
     handleClose();
-  }
+  };
 
-  render() {
-    const {
-      isOpen,
-      anchorEl,
-      options,
-      selectedOption,
-      handleClose,
-      menuStyle,
-    } = this.props;
-
-    return (
-      <Popover
-        open={isOpen}
-        anchorEl={anchorEl}
-        anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        onRequestClose={handleClose}
-      >
-        <Menu style={menuStyle}>
-          {options.map(option => (
-            <MenuItem
-              key={option.value}
-              onClick={() => this.selectItem(option.value)}
-              disabled={option.disabled}
+  return (
+    <Popover
+      open={isOpen}
+      anchorEl={anchorEl}
+      anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+      targetOrigin={{horizontal: 'right', vertical: 'top'}}
+      onRequestClose={handleClose}
+    >
+      <Menu style={menuStyle}>
+        {options.map(option => (
+          <MenuItem
+            key={option.value}
+            onClick={() => selectItem(option.value)}
+            disabled={option.disabled}
+          >
+            <span
+              className={classnames(
+                'overflowPopover__menuItemLabel',
+                {
+                  'overflowPopover__menuItemLabel--disabled': option.disabled,
+                },
+                {
+                  'overflowPopover__menuItemLabel--selected':
+                    selectedOption.value === option.value,
+                }
+              )}
             >
-              <span
-                className={classnames(
-                  'overflowPopover__menuItemLabel',
-                  {
-                    'overflowPopover__menuItemLabel--disabled': option.disabled,
-                  },
-                  {
-                    'overflowPopover__menuItemLabel--selected':
-                      selectedOption.value === option.value,
-                  }
-                )}
-              >
-                {option.label.toUpperCase()}
-              </span>
-            </MenuItem>
-          ))}
-        </Menu>
-      </Popover>
-    );
-  }
+              {option.label.toUpperCase()}
+            </span>
+          </MenuItem>
+        ))}
+      </Menu>
+    </Popover>
+  );
 }
 
 OverflowPopover.propTypes = {
