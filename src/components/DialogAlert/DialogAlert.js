614f865c1552069f215119bea656f07ece72b647
Sean Groff <sean.groff@selectquote.com>
2020-02-24 16:22:13 -0600

    fix(dialogalert): dialog components for alerts

---
+++ src/components/DialogAlert/DialogAlert.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import RoundedButton from '../RoundedButton';
+import './DialogAlert.css';
+
+const Transition = React.forwardRef((props, ref) => {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+function DialogAlert({
+  secondaryButtonText,
+  children,
+  isDisabled = false,
+  isOpen,
+  onSecondaryButtonClick,
+  onPrimaryButtonClick,
+  primaryButtonText,
+  title,
+}) {
+  const shouldRenderSecondaryButton = React.useMemo(
+    () => secondaryButtonText && onSecondaryButtonClick,
+    [onSecondaryButtonClick, secondaryButtonText]
+  );
+  const shouldRenderPrimaryButton = React.useMemo(
+    () => primaryButtonText && onPrimaryButtonClick,
+    [onPrimaryButtonClick, primaryButtonText]
+  );
+
+  return (
+    <Dialog open={isOpen} TransitionComponent={Transition}>
+      <DialogTitle disableTypography={true} className="dialogAlert__title">
+        {title}
+      </DialogTitle>
+      <DialogContent dividers={true} className="dialogAlert__content">
+        {children}
+      </DialogContent>
+      <DialogActions className="dialogAlert__actions">
+        {shouldRenderSecondaryButton && (
+          <span className="dialogAlert__secondary">
+            <RoundedButton
+              onClick={onSecondaryButtonClick}
+              color="secondary"
+              variant="outlined"
+            >
+              {secondaryButtonText}
+            </RoundedButton>
+          </span>
+        )}
+        {shouldRenderPrimaryButton && (
+          <span className="dialogAlert__primary">
+            <RoundedButton
+              isDisabled={isDisabled}
+              onClick={onPrimaryButtonClick}
+              color="primary"
+            >
+              {primaryButtonText}
+            </RoundedButton>
+          </span>
+        )}
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+DialogAlert.propTypes = {
+  /** The content to be rendered in the dialog body */
+  children: PropTypes.node.isRequired,
+  /** The current disabled state of the Dialog Primary Button */
+  isDisabled: PropTypes.bool,
+  /** The current open/closed state of the Dialog */
+  isOpen: PropTypes.bool.isRequired,
+  /** The primary button text (Button located on right side of Dialog) */
+  primaryButtonText: PropTypes.string,
+  /** Callback function invoke when the user clicks the primary button */
+  onPrimaryButtonClick: PropTypes.func,
+  /** The secondary button text (Button located on left side of Dialog) */
+  secondaryButtonText: PropTypes.string,
+  /** Callback function invoked when the user clicks on the secondary button or outside the Dialog */
+  onSecondaryButtonClick: PropTypes.func,
+  /** Title text at the top of the Dialog */
+  title: PropTypes.string.isRequired,
+};
+
+export default DialogAlert;
