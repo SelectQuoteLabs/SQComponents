d8eb7d63837880391ea788bcb5604659ef5a3101
lucas.homer <lucas.homer@selectquote.com>
2020-03-04 23:10:03 +0000

    feat(tooltip): implement base level Tooltip component
    This Tooltip component extends the MUI Tooltip and sets a few SelectCare styles, including jetBlack
    background-color and making the tooltip itself hoverable in addition to the child component(s).
    
    SSC-20

--- notes/Tooltip.md
+++ notes/Tooltip.md
@@ -1,13 +1,11 @@
 # Tooltip
-
--- Tooltip
+Pro tip - use a tooltip.
 
 ## Design Notes
-
---
+- The default MUI version will close the tooltip after the cursor is no longer hovered over the child element. We are overriding that to easily make the child element a button or more complex and specifically styled text (agent instructions, for example).
+- Default font-size is 1rem (16px)
 
 ## Technical Notes
-## TO DO - UPDATE THIS SECTION
--- If you exclude the Primary button text prop `primaryButtonText` or the Primary button callback function `onPrimaryButtonClick` the Primary Button WILL NOT render.
--- Same rule applies for the Secondary button.
--- This is how to control what buttons render in the component
+- The title prop must either be a string or a DOM element.
+- If you set the title prop to an empty string, the tooltip itself will not render. Do not set it to null or undefined as that will render the arrow and nothing else 👎.
+
--- stories/Tooltip.stories.js
+++ stories/Tooltip.stories.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import {withKnobs} from '@storybook/addon-knobs';
-import {withInfo} from '@storybook/addon-info';
 import markdown from '../notes/Tooltip.md';
 
 import Tooltip from '../src/components/Tooltip';
 
 export default {
   title: 'Tooltip',
-  decorators: [withInfo, withKnobs],
   parameters: {
     notes: {markdown},
   },
 };
 
-export const tooltip = () => (
-  <>
-    <h1>Tooltip</h1>
-    <Tooltip />
-  </>
-);
+export const tooltip = () => {
+  return (
+    <Tooltip title="Tooltip title prop can be a string">
+      <h2>TEXT</h2>
+    </Tooltip>
+  );
+};
 
-tooltip.story = {
-  name: 'Tooltip',
+export const tooltipWithMoarContent = () => {
+  return (
+    <Tooltip
+      title={
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. In egestas
+          erat imperdiet sed euismod. Adipiscing at in tellus integer.
+        </p>
+      }
+    >
+      <h2>TEXT</h2>
+    </Tooltip>
+  );
+};
+
+export const tooltipWithButtonChild = () => {
+  return (
+    <Tooltip title={<button onClick={() => {}}>BUTTON</button>}>
+      <h2>Tooltip with a button in it!</h2>
+    </Tooltip>
+  );
+};
+
+export const tooltipWithCustomArrowPlacement = () => {
+  return (
+    <Tooltip placement="top" title="Control orientation with placement prop">
+      <h2>TEXT</h2>
+    </Tooltip>
+  );
 };
