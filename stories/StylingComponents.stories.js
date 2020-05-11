e631611ae8d4ed4390642f1870a37d29712989b8
Chris Boyle <chris.boyle@selectquote.com>
2020-05-11 10:11:46 -0500

    docs: added a story to document the styles available

---
+++ stories/StylingComponents.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styling from '../STYLING.md';
+
+export default {
+  title: 'Styling',
+  parameters: {
+    notes: {styling},
+  },
+};
+
+export const docs = () => (
+  <h1>Go to the Notes to view documentation on how to style components.</h1>
+);
+
+docs.story = {
+  name: 'Styles',
+};
