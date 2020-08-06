0876c91d324e97e96168edf3a4555bca97e20555
Reggie Roby <reggie.roby@selectquote.com>
2020-08-06 15:18:00 -0500

    fix(sectionheader): use material's Typography component

--- src/components/SectionHeader/SectionHeader.js
+++ src/components/SectionHeader/SectionHeader.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-const styles = {
-  textDecoration: 'overline',
-  textDecorationColor: '#5B5858',
-  borderBottom: '1px solid var(--color-lightGray)',
-  fontSize: '1.4rem',
-  marginBottom: '1.5rem',
-};
+const useStyles = makeStyles({
+  header: {
+    borderBottom: '1px solid var(--color-lightGray)',
+    fontSize: '1.4rem',
+    marginBottom: '1.5rem',
+    color: '#5B5858'
+  }
+});
 
 function SectionHeader({children}) {
-  return <header style={styles}>{children}</header>;
+  const classes = useStyles();
+  return (
+    <Typography variant="overline">
+      <header className={classes.header}>{children}</header>;
+    </Typography>
+  )
 }
 
 SectionHeader.propTypes = {
