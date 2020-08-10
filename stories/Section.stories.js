dbc15c18e8b0f3a0cc6f227c971961b7e4877e2d
lucas.homer <lucas.homer@selectquote.com>
2020-08-10 15:18:59 -0700

    fix(sectionheader): fix some inconsistencies in sectionheader

--- stories/Section.stories.js
+++ stories/Section.stories.js
@@ -14,7 +14,7 @@ export default {
 
 export const simpleSection = () => (
   <Section>
-    <SectionHeader>Section Header</SectionHeader>
+    <SectionHeader title="Section Header" />
     <SectionBody>
       {/* this part is totally up to the consumer */}
       <div style={{display: 'flex'}}>
@@ -50,26 +50,18 @@ export const simpleSection = () => (
 
 export const sectionWithActionHeader = () => (
   <Section>
-    <SectionHeader>
+    <SectionHeader title="Section Header">
       {/* consumer could get fancy and compose a button into the header */}
-      <div
+      <label
         style={{
+          fontSize: '1rem',
           display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'baseline',
         }}
       >
-        <div>Section Header</div>
-        <label
-          style={{
-            fontSize: '1rem',
-            display: 'flex',
-          }}
-        >
-          An input!
-          <input type="checkbox" />
-        </label>
-      </div>
+        An input!
+        <input type="checkbox" />
+      </label>
+      {/* </div> */}
     </SectionHeader>
     <SectionBody>
       <div style={{display: 'flex'}}>
@@ -106,6 +98,48 @@ export const sectionWithActionHeader = () => (
         urna. Nam eros orci, tincidunt lobortis ex id, eleifend pharetra nibh.
         Fusce aliquam diam in tortor fermentum, vel fermentum enim consectetur.
         Phasellus id ullamcorper eros.
+      </p>
+    </SectionBody>
+  </Section>
+);
+
+export const sectionWithHeaderStyleOverrides = () => (
+  <Section>
+    <SectionHeader
+      title="Section Header"
+      containerStyles={{
+        marginBottom: '5rem',
+        borderTop: '1px solid var(--color-lightGray)',
+      }}
+    />
+    <SectionBody>
+      {/* this part is totally up to the consumer */}
+      <div style={{display: 'flex'}}>
+        <div style={{padding: '1rem', border: '1px solid blue'}}>
+          Do whatever.
+        </div>
+        <div style={{padding: '1rem', border: '1px solid blue'}}>
+          Maybe an input?
+        </div>
+        <div style={{padding: '1rem', border: '1px solid blue'}}>
+          Or some static stuff.
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mattis
+        augue dui, nec posuere eros scelerisque ut. Maecenas in bibendum nisi.
+        Quisque egestas hendrerit justo mollis scelerisque. Ut iaculis blandit
+        magna, eget congue neque bibendum in. Ut dignissim nisl a erat mattis
+        bibendum. Proin at elit et ante tempor maximus vitae vel tortor.
+      </p>
+      <p>
+        Nunc commodo commodo mi, semper feugiat erat. Sed vitae condimentum
+        urna. Nam eros orci, tincidunt lobortis ex id, eleifend pharetra nibh.
+        Fusce aliquam diam in tortor fermentum, vel fermentum enim consectetur.
+        Phasellus id ullamcorper eros. Nulla vel luctus turpis. Donec blandit
+        urna enim. Ut sit amet lacus porttitor, condimentum urna quis, feugiat
+        magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+        posuere cubilia Curae;
       </p>
     </SectionBody>
   </Section>
