79292705fe85257e6c11f82dde4735454fa558ec
Reggie Roby <reggie.roby@selectquote.com>
2020-08-10 09:24:56 -0500

    fix(sectionheader): add typography style

--- src/components/SectionHeader/SectionHeader.js
+++ src/components/SectionHeader/SectionHeader.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
 
 const styles = {
   borderBottom: '1px solid var(--color-lightGray)',
   fontSize: '1.4rem',
   marginBottom: '1.5rem',
+  color: 'var(--color-granite)',
 };
 
-function SectionHeader({children, style}) {
-  return <header style={{...styles, ...style}}>{children}</header>;
+function SectionHeader({children, style = {}}) {
+  return (
+    <Typography
+      variant="overline"
+      component="header"
+      style={{...styles, ...style}}
+    >
+      {children}
+    </Typography>
+  );
 }
 
 SectionHeader.propTypes = {
