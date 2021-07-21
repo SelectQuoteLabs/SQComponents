72f5eedb0c29186dadb5c0f4061db4c7c901f002
Grant Doohen <grant.doohen@selectquote.com>
2021-07-21 09:26:14 -0700

    fix: fixed invalid style in theme.js

--- src/theme.js
+++ src/theme.js
@@ -173,7 +173,7 @@ const theme = {
     MuiIconButton: {
       root: {
         '&:hover': {
-          backgroundColor: 'none',
+          backgroundColor: 'transparent',
         },
       },
     },
